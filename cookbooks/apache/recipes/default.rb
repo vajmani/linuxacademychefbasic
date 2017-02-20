b6a9127d18ea62606a32211b75ee691fdde18340
Virender <virender.ajmani@gmail.com>
2017-02-20 21:02:03 +0000

    added diff version of apache cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,11 +5,11 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 package 'apache2' do
-	package_name 'apache2'
+	package_name 'httpd'
 	action :install
 end
 
 service 'apache2' do
-	service_name 'apache2'
+	service_name 'httpd'
 	action [:start, :enable]
 end
